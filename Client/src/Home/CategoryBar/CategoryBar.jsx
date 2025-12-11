998af2bb10ef26fc20f233b12091642149b2a70e
varunojhaa <varunojha77@gmail.com>
2025-12-11 18:12:07 +0530

    ready all

--- Client/src/Home/CategoryBar/CategoryBar.jsx
+++ Client/src/Home/CategoryBar/CategoryBar.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./CategoryBar.css";
 import { Link } from "react-router-dom";
+import a from '../../assets/minutes.webp';
+import b from '../../assets/mobiles.png';
+import c from '../../assets/Fashion.webp';
+import d from '../../assets/Electronics.webp';
+import e from '../../assets/Home&Furniture.jpg';
+import f from '../../assets/Tv&Appliances.jpg';
+import g from '../../assets/Flight Booking.png';
+import h from '../../assets/Beauty.png';
+import i from '../../assets/Grocery.png';
 
 // The component now accepts a prop `onCategoryHover` to trigger the dropdown.
 export default function CategoryBar({ onCategoryHover }) {
   // We've created an array of objects. Each object has the category name and its specific image source.
   // This is a cleaner way to manage the data.
   const categoryData = [
-    { name: "Minutes", imgSrc: "/src/assets/minutes.webp", pageSrc:"/MinutesPage" },
-    { name: "Mobiles & Tablet ", imgSrc: "/src/assets/mobiles.png", pageSrc:"*" },
-    { name: "Fashion", imgSrc: "/src/assets/Fashion.webp", pageSrc:"*" },
-    { name: "Electronics", imgSrc: "/src/assets/Electronics.webp", pageSrc:"*" },
-    { name: "Home & Furniture", imgSrc: "/src/assets/Home&Furniture.jpg", pageSrc:"*" },
-    { name: "TVs & Appliances", imgSrc: "/src/assets/Tv&Appliances.jpg", pageSrc:"*" },
-    { name: "Flight Bookings", imgSrc: "/src/assets/Flight Booking.png", pageSrc:"*" },
-    { name: "Beauty, Food..", imgSrc: "/src/assets/Beauty.png", pageSrc:"*" },
-    { name: "Grocery", imgSrc: "/src/assets/Grocery.png", pageSrc:"*" },
+    { name: "Minutes", imgSrc: a, pageSrc:"/MinutesPage" },
+    { name: "Mobiles & Tablet ", imgSrc: b, pageSrc:"*" },
+    { name: "Fashion", imgSrc: c, pageSrc:"*" },
+    { name: "Electronics", imgSrc: d, pageSrc:"*" },
+    { name: "Home & Furniture", imgSrc: e, pageSrc:"*" },
+    { name: "TVs & Appliances", imgSrc: f, pageSrc:"*" },
+    { name: "Flight Bookings", imgSrc: g, pageSrc:"*" },
+    { name: "Beauty, Food..", imgSrc: h, pageSrc:"*" },
+    { name: "Grocery", imgSrc: i, pageSrc:"*" },
   ];
 
   return (
